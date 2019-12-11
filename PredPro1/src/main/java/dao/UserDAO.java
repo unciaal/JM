dd28223241f1d29b4a6cde54cccd4864717db9be
Konstantin <tkn81@yandex.ru>
2019-12-12 01:47:01 +0300

    Не большие доработки
    хотя идей еще больше

--- PredPro1/src/main/java/dao/UserDAO.java
+++ PredPro1/src/main/java/dao/UserDAO.java
@@ -71,16 +71,11 @@ public class UserDAO {
     }
 
     public List<User> selectAllUsers() {
-        // using try-with-resources to avoid closing resources (boiler plate code)
         List<User> users = new ArrayList<>();
-        // Step 1: Establishing a Connection
         try (Connection connection = getConnection();
-             // Step 2:Create a statement using connection object
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
             System.out.println(preparedStatement);
-            // Step 3: Execute the query or update query
             ResultSet rs = preparedStatement.executeQuery();
-            // Step 4: Process the ResultSet object.
             while (rs.next()) {
                 long id = rs.getLong("id");
                 String name = rs.getString("name");
