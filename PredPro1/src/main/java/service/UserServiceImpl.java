6923d18c4c3d27a082c56bbd8f015a900b69b8e5
Konstantin <tkn81@yandex.ru>
2019-12-22 03:28:15 +0300

    доработал год согласно замечанием

--- PredPro1/src/main/java/service/UserServiceImpl.java
+++ PredPro1/src/main/java/service/UserServiceImpl.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface UserServiceImpl {
-    public List<User> selectAllUsers() throws SQLException;
-    public void insertUser(User user) throws SQLException;
-    public User selectUser(long id) throws SQLException;
-    public boolean deleteUser(long id) throws SQLException;
-    public boolean updateUser(User user) throws SQLException;
+     List<User> selectAllUsers() throws SQLException;
+     void insertUser(User user) throws SQLException;
+     User selectUser(long id) throws SQLException;
+     boolean deleteUser(long id) throws SQLException;
+     boolean updateUser(User user) throws SQLException;
 }
