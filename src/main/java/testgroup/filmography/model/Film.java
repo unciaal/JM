c738c19e6156c0efb1dc3238bc320cad67376f1a
Konstantin <tkn81@yandex.ru>
2020-01-11 15:32:43 +0300

    crud

--- src/main/java/testgroup/filmography/model/Film.java
+++ src/main/java/testgroup/filmography/model/Film.java
@@ -7,20 +7,20 @@ import javax.persistence.*;
 public class Film {
 
     @Id
-    @Column(name = "id")
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "title")
+
     private String title;
 
-    @Column(name = "year")
+
     private int year;
 
-    @Column(name = "genre")
+
     private String genre;
 
-    @Column(name = "watched")
+
     private boolean watched;
 
 
